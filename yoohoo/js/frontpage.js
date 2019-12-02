767edb27de604416fb2b5f899ee2babcdf45446d
Jonah McKay <jonah@amimckay.com>
2019-12-02 11:00:32 -0500

    login front page, signup page, guided tour start

---
+++ yoohoo/js/frontpage.js
@@ -0,0 +1,12 @@
+function setTempUsername(nameGiven)
+{
+	Cookies.set("Tempusername", nameGiven, {expires: 6000 });
+}
+
+function login(nameGiven)
+{
+	if (Cookies.get("username") != null)
+	{
+		
+	}
+}
