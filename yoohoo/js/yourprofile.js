9f145b9dbab0d6b0d8cd2339a4910d2acfcc7293
Jonah McKay <jonah@amimckay.com>
2019-12-07 13:45:35 -0500

    added DOB and interests to yourprofile

--- yoohoo/js/yourprofile.js
+++ yoohoo/js/yourprofile.js
@@ -1,4 +1,33 @@
 document.getElementById('profilename').innerHTML = Cookies.get("realname");
 document.getElementById('age').innerHTML = Cookies.get("age");
-document.getElementById('interests').innerHTML = Cookies.get("interests");
+document.getElementById('birthday').innerHTML = Cookies.get("birthday");
+var interests = "";
+var interestData = Cookies.get("interests");
+if (interestData.charAt(0) == "1")
+{
+  interests += "Cooking, "
+}
+if (interestData.charAt(1) == "1")
+{
+  interests += "Art, "
+}
+if (interestData.charAt(2) == "1")
+{
+  interests += "Technology, "
+}
+if (interestData.charAt(3) == "1")
+{
+  interests += "Travel, "
+}
+if (interestData.charAt(4) == "1")
+{
+  interests += "News, "
+}
+if (interests.length > 0)
+{
+  //remove comma and space
+  interests = interests.slice(0, -2);
+}
+alert(interestData);
+document.getElementById('interests').innerHTML = interests;
 document.getElementById('yourFirstPost').innerHTML = Cookies.get("realname")+ ' gained 7 social points for creating an account on ' + Date();
