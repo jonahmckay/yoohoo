7ff4283163dc4358734ea15988004d0ba4415818
AudreyCoulombe <coulombe.audrey@example.com>
2019-12-06 23:47:44 -0500

    changed "your profile"

--- yoohoo/js/yourprofile.js
+++ yoohoo/js/yourprofile.js
@@ -1,1 +1,4 @@
 document.getElementById('profilename').innerHTML = Cookies.get("realname");
+document.getElementById('age').innerHTML = Cookies.get("age");
+document.getElementById('interests').innerHTML = Cookies.get("interests");
+document.getElementById('yourFirstPost').innerHTML = Cookies.get("realname")+ ' gained 7 social points for creating an account on ' + Date();
